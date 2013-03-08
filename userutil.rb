54cd246e84dd87fecb368a672a10ee7448c4a3c9
Koga Osamu(osa_k) <osak.63@gmail.com>
2013-03-08 21:58:23 +0900

    Now filter returns User object.

--- userutil.rb
+++ userutil.rb
@@ -1,26 +1,22 @@
 # -*- coding: utf-8 -*-
 
 Plugin.create(:userutil) do
-  def id_names(users)
-    users.map{|u| u[:idname]}
-  end
-
   on_followings_created do |service, users|
-    @name_list.push(*id_names(users))
+    @user_list.push(*users)
   end
 
   on_followings_destroy do |service, users|
-    @name_list -= id_names(users)
+    @user_list -= users
   end
 
-  filter_usercomplete do |partial, buf|
-    [partial, buf.push(*@name_list.select{|name| name.start_with?(partial)})]
+  filter_user_prefix_search do |partial, buf|
+    [partial, buf.push(*@user_list.select{|user| user[:idname].start_with?(partial)})]
   end
 
-  @name_list = []
+  @user_list = []
   Delayer.new {
     Service.primary.followings(cache: true).next { |users|
-      @name_list.push(*id_names(users))
+      @user_list = users.dup
     }
   }
 end
